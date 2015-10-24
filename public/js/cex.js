313ff0a9588879ee5c6c1985d66d425f9fffdbf9
xnieamo <xiaomao_ding@yahoo.com>
2015-10-24 14:52:49 -0400

    whut

--- public/js/cex.js
+++ public/js/cex.js
@@ -1,10 +1,15 @@
 
 function triainglesBitches() {
   var container = document.getElementById('container');
-  var pattern = Trianglify({
+/*   var pattern = Trianglify({
       width: window.innerWidth,
       height: window.innerHeight
   });
-  document.body.appendChild(pattern.canvas());
-  Trianglify({variance: "1", seed: 'wv44w', x_colors: 'random'}).canvas()
+  document.body.appendChild(pattern.canvas()); */
+document.body.appendChild(Trianglify({variance: "1",
+  seed: 'wv44w',
+  x_colors: 'YlGnBu',
+  width: window.innerWidth,
+  height: window.innerHeight
+}).canvas());
 }
