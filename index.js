6c8e3b9df0e8e849017cc3d5138eb960ad61f795
Eva Snyder <snyde24e@mtholyoke.edu>
2015-10-24 16:40:36 -0400

    Dual Screen

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
- 
+
 // Initialize appication with route / (that means root of the application)
 app.get('/', function(req, res){
   var express=require('express');
@@ -15,9 +15,9 @@ app.get('/index.html', function(req, res){
   app.use(express.static(path.join(__dirname)));
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
- 
+
 // Register events on socket connection
-io.on('connection', function(socket){ 
+io.on('connection', function(socket){
   socket.on('chatMessage', function(from, msg){
     io.emit('chatMessage', from, msg);
   });
@@ -25,8 +25,8 @@ io.on('connection', function(socket){
     io.emit('notifyUser', user);
   });
 });
- 
+
 // Listen application request on port 3000
-http.listen(process.env.port, function(){
-  console.log('listening on *:3000');
+http.listen(3000, function(){
+  console.log('listening on :3000');
 });
--- public/js/cex.js
+++ public/js/cex.js
@@ -1,3 +1,5 @@
+var login = true;
+
 
 function triainglesBitches() {
   var container = document.getElementById('container');
@@ -12,4 +14,23 @@ document.body.appendChild(Trianglify({variance: "1",
   width: window.innerWidth,
   height: window.innerHeight
 }).canvas());
+}
+
+function changeScreen() {
+  if(login) {
+    $('#container').addClass('.inactiveDiv');
+    $('#container').removeClass('.activeDiv');
+
+    $('#chat_container').removeClass('inactiveDiv');
+    $('#chat_container').addClass('.activeDiv');
+
+    login = false;
+  } else {
+    $('#chat_container').removeClass('.activeDiv');
+    $('#chat_container').addClass('.inactiveDiv');
+
+    $('#container').removeClass('inactiveDiv');
+    $('#container').addClass('.activeDiv');
+    login = true;
+  }
 }
