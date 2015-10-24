3fd7499c45fda6388e143eac47203d17462d851f
Eva Snyder <snyde24e@mtholyoke.edu>
2015-10-24 16:42:27 -0400

    port

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ io.on('connection', function(socket){
 });
 
 // Listen application request on port 3000
-http.listen(3000, function(){
+http.listen(process.env.port, function(){
   console.log('listening on :3000');
 });
