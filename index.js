a556bab25599e1396ad9b89b6da162abc6609552
xnieamo <xiaomao_ding@yahoo.com>
2015-10-24 23:38:55 -0400

    >>>

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
+var watson = require('watson-developer-cloud');
+var testMod = require('./public/js/testModule.js');
+
 var prevFrom = '';
 var inPause = false;
+
+var speech_to_text = watson.speech_to_text({
+	username: '190f202e-aaa7-4849-975d-70e3a14b51b9',
+	password: 'clyAOaAmyl3h',
+	version: 'v1'
+});
+
+function processAudio(){
+	
+}
 
 // Initialize appication with route / (that means root of the application)
 app.get('/', function(req, res){
@@ -21,6 +34,9 @@ app.get('/index.html', function(req, res){
 // Register events on socket connection
 io.on('connection', function(socket){
   socket.on('connectMessage', function(from, msg){
+	  console.log('hello');
+	  console.log(testMod.getX());
+
 	  io.emit('connectMessage', from, msg);
   });
 
---
+++ public/js/testModule.js
@@ -0,0 +1,7 @@
+var x = 10;
+
+var getX = function() {
+	return x;
+};
+
+module.exports.getX = getX;
