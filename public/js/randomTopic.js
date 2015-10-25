0e175a53c7b97fb5fe19fa1a81b040367b470343
xnieamo <xiaomao_ding@yahoo.com>
2015-10-25 13:50:18 -0400

    asdf

--- public/js/randomTopic.js
+++ public/js/randomTopic.js
@@ -3,7 +3,7 @@ var topics = ["The 80's", "Country", "Elvis Presley", "Top Hits", "Taylor Swift"
 
 exports.getTopic = function(){
 		//console.log('imherer');
-		return topics[Math.floor((Math.random() * 15))];
+		return topics[Math.floor((Math.random() * topics.length))];
 		// document.getElementById('topic').innerHTML = topics[Math.floor((Math.random() * 5))];
   };
   // module.exports.getTopic = getTopic;
