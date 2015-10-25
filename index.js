b3c9634d334168c1351b27c0b6750048e64ded31
Cam <zawacki.cameron@gmail.com>
2015-10-24 22:40:39 -0400

    angular

--- index.js
+++ index.js
@@ -32,6 +32,20 @@ io.on('connection', function(socket){
     socket.on('PauseExit', function(isInPause){
 	  inPause = isInPause;
   });
+
+  socket.on('SomeoneGetsAPoint', function(from){
+    for(int i = 0; i < players.length; i++){
+      if(players[i] === from){
+          scores[i]++;
+          if(i == 0){
+            document.getElementById('Ascore').innerHTML = scores[i];
+          }else{
+            document.getElementById('Bscore').innerHTML = scores[i];
+
+          }
+      }
+    }
+  });
   
   socket.on('chatMessage', function(from, msg){
 	  if (prevFrom !== from && !inPause){
