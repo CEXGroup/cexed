913dc7737cddd24c8a1aafbf11575e11edcc12d7
Molly Zawacki <Molly Zawacki>
2015-10-25 06:04:51 -0400

    fG

--- public/js/randomTopic.js
+++ public/js/randomTopic.js
@@ -1,12 +1,11 @@
 
-var topics = ["America", "Cows", "Party Animals", "Children", "Pizza"];
+var topics = ["The 80's", "Love", "Country", "Parties", "Elvis Presley","Booty","Booty", "Travel", "Taylor Swift", "The Beatles", "Drugs","TV Shows", "Children's Songs", "Movies", "Death"];
 
 exports.getTopic = function(){
 		//console.log('imherer');
-		return topics[Math.floor((Math.random() * 5))];
+		return topics[Math.floor((Math.random() * 15))];
 		// document.getElementById('topic').innerHTML = topics[Math.floor((Math.random() * 5))];
 
 
   };
-
   // module.exports.getTopic = getTopic;
